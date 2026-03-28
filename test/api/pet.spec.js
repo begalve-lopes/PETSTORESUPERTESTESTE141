12a95976504c5a2436ab3fc087db2020c5e16950
begalve-lopes <begalve.927390438.00@gmail.com>
2026-03-29 00:58:32 +0100

    Data-driven com Supertest

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -3,7 +3,10 @@ const supertest = require("supertest");
 const createdPetId = 173218101;
 
 describe("API PetStore Swagger - Entidade Pet", () => {
+  jest.setTimeout(20000);
   const request = supertest("https://petstore.swagger.io/v2");
+  const petDDT = require("../../vendors/json/petDDT.json");
+  const petUpdateDDT = require("../../vendors/json/petUpdateDDT.json");
 
   it("Post Pet", () => {
     const pet = require("../../vendors/json/pet.json");
@@ -24,33 +27,87 @@ describe("API PetStore Swagger - Entidade Pet", () => {
     return request
       .put("/pet")
       .send(pet)
-      .then((response)=>{
+      .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.body.id).toBe(createdPetId);
         expect(response.body.name).toBe("dog");
         expect(response.body.category.name).toBe("begas");
         expect(response.body.tags[0].name).toBe("pending");
+      });
+  });
+
+  it("Get Pet", () => {
+    return request.get(`/pet/${createdPetId}`).then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toBe(createdPetId);
+      expect(response.body.status).toBe("available");
+    });
+  });
+
+  it("Delete Pet", () => {
+    return request.delete(`/pet/${createdPetId}`).then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body.code).toBe(200);
+      expect(response.body.type).toBe("unknown");
+      expect(response.body.message).toBe(`${createdPetId}`);
+    });
+  });
+
+  it.each(petDDT)("Post Pet DDT: %s", async (data) => {
+    return request
+      .post("/pet")
+      .send({
+        id: data.idPet,
+        name: data.namePet,
+        category: {
+          id: data.category_id,
+          name: data.category_name,
+        },
       })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.id).toBe(data.idPet);
+        expect(response.body.name).toBe(data.namePet);
+        expect(response.body.category.id).toBe(data.category_id);
+        expect(response.body.category.name).toBe(data.category_name);
+      });
   });
 
-  it("Get Pet",() => {
-    return request
-        .get(`/pet/${createdPetId}`)
-        .then((response)=>{
-            expect(response.statusCode).toBe(200)
-            expect(response.body.id).toBe(createdPetId)
-            expect(response.body.status).toBe("available")
-        })
+  it.each(petDDT)("Get Pet DDT: %s", async (data) => {
+    return (reponse = request.get(`/pet/${data.idPet}`).then((response) => {
+      {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.id).toBe(data.idPet);
+      }
+    }));
   });
 
-  it("Delete Pet",()=>{
+  it.each(petUpdateDDT)("Update Pet DDT: %s", async (data) => {
     return request
-        .delete(`/pet/${createdPetId}`)
-        .then((response)=>{
-            expect(response.statusCode).toBe(200)
-            expect(response.body.code).toBe(200)
-            expect(response.body.type).toBe("unknown")
-            expect(response.body.message).toBe(`${createdPetId}`)
-        })
+      .put("/pet")
+      .send({
+        id: data.idPet,
+        name: data.namePet,
+        category: {
+          id: data.category_id,
+          name: data.category_name,
+        },
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.id).toBe(data.idPet);
+        expect(response.body.name).toBe(data.namePet);
+        expect(response.body.category.id).toBe(data.category_id);
+        expect(response.body.category.name).toBe(data.category_name);
+      });
+  });
+
+  it.each(petDDT)("Delete Pet DDT: %s",async(data)=>{
+    return request.delete(`/pet/${data.idPet}`).then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body.code).toBe(200);
+      expect(response.body.type).toBe("unknown");
+      expect(response.body.message).toBe(`${data.idPet}`);
+    })
   })
 });
